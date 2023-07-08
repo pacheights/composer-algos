d95ac2738a522f3fe5fca9cb9455fb373a6ebd43
Pearson Buck <pearsonhbuck@gmail.com>
2023-07-08 10:51:21 -0700

    new algo

--- src/algos.ts
+++ src/algos.ts
@@ -42,75 +42,93 @@ export const algo = (
 
     const bondRSI = bondRSIs[i - 1];
     const leveragedRSI = leveragedRSIs[i - 1];
+    const inverseRSI = 100 - leveragedRSI;
 
     const leveragedTwoHundredDayMA = leveragedMovingAverages[i - 1];
     const leveragedTwentyDayMA = leveragedTwentyDayMovingAverages[i - 1];
     const leveragedPrice = buyHold2x[i - 1];
 
+    const previousDayReturn = calculateReturn(sp500[i - 2], sp500[i - 1]);
     const dailyReturn = calculateReturn(sp500[i - 1], sp500[i]);
     const bondReturn = calculateReturn(bonds[i - 1], bonds[i]);
     const inverseReturn = -1 * dailyReturn;
     let algoReturn: number = dailyReturn;
 
-    if (leveragedPrice > leveragedTwoHundredDayMA) {
-      if (sp500RSI > 79) {
-        algoReturn = 0;
-      } else {
-        if (bondRSI > sp500RSI) {
-          algoReturn = dailyReturn;
-        } else {
-          algoReturn = bondReturn;
-        }
-      }
+    if (previousDayReturn > 0.03) {
+      algoReturn = inverseReturn;
     } else {
-      if (sp500RSI < 30) {
-        algoReturn = dailyReturn;
-      } else {
-        if (leveragedRSI < 30) {
-          algoReturn = dailyReturn;
+      if (leveragedPrice > leveragedTwoHundredDayMA) {
+        if (sp500RSI > 82) {
+          algoReturn = inverseReturn;
         } else {
-          if (leveragedPrice > leveragedTwentyDayMA) {
+          if (bondRSI > sp500RSI) {
             algoReturn = dailyReturn;
           } else {
-            if (bondRSI > 100 - sp500RSI) {
-              algoReturn = inverseReturn;
+            algoReturn = bondReturn;
+          }
+        }
+      } else {
+        if (sp500RSI < 31) {
+          algoReturn = dailyReturn;
+        } else {
+          if (leveragedRSI < 31) {
+            algoReturn = dailyReturn;
+          } else {
+            if (leveragedPrice > leveragedTwentyDayMA) {
+              if (bondRSI > sp500RSI) {
+                algoReturn = dailyReturn;
+              } else {
+                algoReturn = bondReturn;
+              }
             } else {
-              algoReturn = bondReturn;
+              if (inverseRSI > bondRSI) {
+                algoReturn = inverseReturn;
+              } else {
+                algoReturn = bondReturn;
+              }
             }
           }
         }
       }
     }
 
-    // if (leveragedPrice > leveragedTwoHundredDayMA) {
-    //   if (sp500RSI > 79) {
-    //     algoReturn = bondReturn;
-    //   } else {
-    //     if (bondRSI > sp500RSI) {
-    //       algoReturn = dailyReturn;
-    //     } else {
-    //       algoReturn = bondReturn;
-    //     }
-    //   }
-    // } else {
-    //   if (sp500RSI < 30) {
-    //     algoReturn = dailyReturn;
-    //   } else {
-    //     if (leveragedRSI < 30) {
-    //       algoReturn = dailyReturn;
-    //     } else {
-    //       if (leveragedPrice > leveragedTwentyDayMA) {
-    //         if (bondRSI > 100 - sp500RSI) {
-    //           algoReturn = inverseReturn;
-    //         } else {
-    //           algoReturn = bondReturn;
-    //         }
-    //       } else {
-    //         algoReturn = bondReturn;
-    //       }
-    //     }
-    //   }
-    // }
+    if (previousDayReturn > 0.03) {
+      algoReturn = inverseReturn;
+    } else {
+      if (sp500RSI > 82) {
+        algoReturn = inverseReturn;
+      } else {
+        if (sp500RSI < 31) {
+          algoReturn = dailyReturn;
+        } else {
+          if (leveragedRSI < 31) {
+            algoReturn = dailyReturn;
+          } else {
+            if (leveragedPrice > leveragedTwoHundredDayMA) {
+              if (bondRSI > sp500RSI) {
+                algoReturn = dailyReturn;
+              } else {
+                algoReturn = bondReturn;
+              }
+            } else {
+              if (leveragedPrice > leveragedTwentyDayMA) {
+                if (bondRSI > sp500RSI) {
+                  algoReturn = dailyReturn;
+                } else {
+                  algoReturn = bondReturn;
+                }
+              } else {
+                if (inverseRSI > bondRSI) {
+                  algoReturn = 0;
+                } else {
+                  algoReturn = bondReturn;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
 
     const previousBalance = dailyBalances[j - 1];
     j++;
